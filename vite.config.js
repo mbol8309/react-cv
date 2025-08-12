0e099ffa187049052b08020a5d39c0ed36538bee
Miguel Bolivar <mbol8309@gmail.com>
2025-08-12 16:56:49 +0200

    add version

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import AutoVersion from "vite-plugin-auto-version";
 
 // https://vite.dev/config/
 export default defineConfig({
     base: '/react-cv/', // Set the base path for GitHub Pages
-  plugins: [react()],
+    plugins: [react(),
+    AutoVersion({ insertCheck: true, refreshTime: 10 * 60 * 1000 }),
+    ],
 })
